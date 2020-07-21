fa0fbad1dbd38d7fcb2cb6c51caab9471149ed3b
Huynh Hoang Phuc <62494021+HuynhHoangPhuc@users.noreply.github.com>
2020-07-21 08:47:13 +0700

    Update canonLBP2900.sh

--- canonLBP2900.sh
+++ canonLBP2900.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-sudo apt -y install build-essential git autoconf libtool libcups2-dev libcupsimage2-dev
+sudo apt -y install build-essential git autoconf libtool libcups2-dev libcupsimage2-dev ttf-mscorefonts-installer
+sudo fc-cache -f -v
 git clone https://github.com/agalakhov/captdriver.git
 cd captdriver
 autoreconf -i
