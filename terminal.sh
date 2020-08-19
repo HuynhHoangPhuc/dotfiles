879540bab149b6a9b76009372875df528819c4c5
Huynh Hoang Phuc <62494021+HuynhHoangPhuc@users.noreply.github.com>
2020-08-19 18:24:01 +0700

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -135,7 +135,7 @@ function install_yarn() {
 
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt update && sudo apt install yarn
+    sudo apt update && sudo apt install --no-install-recommends yarn
     
     echo "Install yarn ... Done"
 }
