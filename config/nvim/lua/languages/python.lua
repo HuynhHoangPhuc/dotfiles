af2691d430c460bdb978d53611f11084ed6c3d95
Huynh Hoang Phuc <62494021+HuynhHoangPhuc@users.noreply.github.com>
2025-08-09 11:48:32 +0700

    Neovim: Python config

---
+++ config/nvim/lua/languages/python.lua
@@ -0,0 +1,64 @@
+local M = {}
+
+M.lsp = {
+	servers = {
+		ruff = {},
+		pyright = {
+			settings = {
+				pyright = {
+					-- Using Ruff's import organizer
+					disableOrganizeImports = true,
+				},
+				python = {
+					analysis = {
+						-- Ignore all files for analysis to exclusively use Ruff for linting
+						ignore = { "*" },
+					},
+				},
+			},
+		},
+	},
+
+	setup = {
+		ruff = function(_, _)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup(
+					"lsp_attach_disable_ruff_hover",
+					{ clear = true }
+				),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client == nil then
+						return
+					end
+					if client.name == "ruff" then
+						-- Disable hover in favor of Pyright
+						client.server_capabilities.hoverProvider = false
+					end
+				end,
+				desc = "LSP: Disable hover capability from Ruff",
+			})
+		end,
+	},
+}
+
+M.format = {
+	formatters_by_ft = {
+		python = {
+			-- To fix auto-fixable lint errors.
+			"ruff_fix",
+			-- To run the Ruff formatter.
+			"ruff_format",
+			-- To organize the imports.
+			"ruff_organize_imports",
+		},
+	},
+}
+
+M.lint = {
+	linters_by_ft = {
+		python = { "ruff" },
+	},
+}
+
+return M
