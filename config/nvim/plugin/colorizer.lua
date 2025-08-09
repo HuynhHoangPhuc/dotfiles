50e2fbc9b740977334cd499b07997ad101b1dcc6
Huynh Hoang Phuc <62494021+HuynhHoangPhuc@users.noreply.github.com>
2025-08-09 11:50:07 +0700

    Neovim: color highlight helper

---
+++ config/nvim/plugin/colorizer.lua
@@ -0,0 +1,26 @@
+require("colorizer").setup({
+	filetypes = {
+		"html",
+		"css",
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+		"astro",
+	},
+	user_default_options = {
+		css = true,
+		css_fn = true,
+		tailwind = "both",
+		tailwind_opts = {
+			update_names = true,
+		},
+		sass = {
+			enable = true,
+			parsers = { "css" },
+		},
+		mode = "virtualtext",
+		virtualtext = "󱓻",
+		virtualtext_inline = "before",
+	},
+})
