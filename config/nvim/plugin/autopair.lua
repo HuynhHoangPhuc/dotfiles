094a6801b66535f3fb4de529f80b0afd56898f13
Huynh Hoang Phuc <62494021+HuynhHoangPhuc@users.noreply.github.com>
2025-08-09 11:50:21 +0700

    Neovim: automatic pairing

---
+++ config/nvim/plugin/autopair.lua
@@ -0,0 +1,17 @@
+local npairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
+local conds = require("nvim-autopairs.conds")
+
+npairs.setup()
+
+-- Autoclosing angle-brackets.
+npairs.add_rule(Rule("<", ">", {
+	-- Avoid conflicts with nvim-ts-autotag.
+	"-html",
+	"-javascriptreact",
+	"-typescriptreact",
+}):with_pair(conds.before_regex("%a+:?:?$", 3)):with_move(function(opts)
+	return opts.char == ">"
+end))
+
+require("nvim-ts-autotag").setup()
