9b5b0bed98f656940c71c87c52450fc4fb3dee20
Huynh Hoang Phuc <62494021+HuynhHoangPhuc@users.noreply.github.com>
2025-08-10 09:46:31 +0700

    Nix: set default shell for tmux

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -7,6 +7,7 @@
   programs.tmux = {
     enable = true;
     extraConfig = builtins.readFile ../config/tmux/tmux.conf;
+    shell = "${pkgs.zsh}/bin/zsh";
     plugins = with pkgs.tmuxPlugins; [
       sensible
       yank
