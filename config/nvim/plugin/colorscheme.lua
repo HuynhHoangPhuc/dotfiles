7af38853e297874d4b61caa71d86d89261225c63
Phuc Tran <phuc@myrmex.dev>
2026-03-06 08:41:35 +0000

    chore: change default color to deeply dark, fix live_grep keymap

--- config/nvim/plugin/colorscheme.lua
+++ config/nvim/plugin/colorscheme.lua
@@ -1,14 +1,21 @@
 require("catppuccin").setup({
-	integrations = {
-		fzf = true,
-		mini = { enable = true },
-		-- copilot_vim = true,
-	},
-	styles = {
-		conditionals = { "italic" },
-		keywords = { "italic" },
-		loops = { "italic" },
-	},
+  integrations = {
+    fzf = true,
+    mini = { enable = true },
+    -- copilot_vim = true,
+  },
+  styles = {
+    conditionals = { "italic" },
+    keywords = { "italic" },
+    loops = { "italic" },
+  },
+  color_overrides = {
+    mocha = {
+      base = "#000000",
+      mantle = "#000000",
+      crust = "#000000",
+    },
+  }
 })
 
 vim.cmd.colorscheme("catppuccin")
--- config/nvim/plugin/picker.lua
+++ config/nvim/plugin/picker.lua
@@ -13,67 +13,67 @@ config.defaults.keymap.builtin["<c-f>"] = "preview-page-down"
 config.defaults.keymap.builtin["<c-b>"] = "preview-page-up"
 
 fzf.setup({
-	{ "default-title" },
-	files = {
-		cwd_prompt = false,
-		actions = {
-			["alt-i"] = { actions.toggle_ignore },
-			["alt-h"] = { actions.toggle_hidden },
-		},
-	},
-	grep = {
-		actions = {
-			["alt-i"] = { actions.toggle_ignore },
-			["alt-h"] = { actions.toggle_hidden },
-		},
-	},
+  { "default-title" },
+  files = {
+    cwd_prompt = false,
+    actions = {
+      ["alt-i"] = { actions.toggle_ignore },
+      ["alt-h"] = { actions.toggle_hidden },
+    },
+  },
+  grep = {
+    actions = {
+      ["alt-i"] = { actions.toggle_ignore },
+      ["alt-h"] = { actions.toggle_hidden },
+    },
+  },
 })
 
 require("fzf-lua").register_ui_select(function(fzf_opts, items)
-	return vim.tbl_deep_extend("force", fzf_opts, {
-		prompt = " ",
-		winopts = {
-			title = " " .. vim.trim(
-				(fzf_opts.prompt or "Select"):gsub("%s*:%s*$", "")
-			) .. " ",
-			title_pos = "center",
-		},
-	}, fzf_opts.kind == "codeaction" and {
-		winopts = {
-			layout = "vertical",
-			-- height is number of items minus 15 lines for the preview, with a max of 80% screen height
-			height = math.floor(
-				math.min(vim.o.lines * 0.8 - 16, #items + 2) + 0.5
-			) + 16,
-			width = 0.5,
-			preview = not vim.tbl_isempty(
-				vim.lsp.get_clients({ bufnr = 0, name = "vtsls" })
-			) and {
-				layout = "vertical",
-				vertical = "down:15,border-top",
-				hidden = "hidden",
-			} or {
-				layout = "vertical",
-				vertical = "down:15,border-top",
-			},
-		},
-	} or {
-		winopts = {
-			width = 0.5,
-			-- height is number of items, with a max of 80% screen height
-			height = math.floor(math.min(vim.o.lines * 0.8, #items + 2) + 0.5),
-		},
-	})
+  return vim.tbl_deep_extend("force", fzf_opts, {
+    prompt = " ",
+    winopts = {
+      title = " " .. vim.trim(
+        (fzf_opts.prompt or "Select"):gsub("%s*:%s*$", "")
+      ) .. " ",
+      title_pos = "center",
+    },
+  }, fzf_opts.kind == "codeaction" and {
+    winopts = {
+      layout = "vertical",
+      -- height is number of items minus 15 lines for the preview, with a max of 80% screen height
+      height = math.floor(
+        math.min(vim.o.lines * 0.8 - 16, #items + 2) + 0.5
+      ) + 16,
+      width = 0.5,
+      preview = not vim.tbl_isempty(
+        vim.lsp.get_clients({ bufnr = 0, name = "vtsls" })
+      ) and {
+        layout = "vertical",
+        vertical = "down:15,border-top",
+        hidden = "hidden",
+      } or {
+        layout = "vertical",
+        vertical = "down:15,border-top",
+      },
+    },
+  } or {
+    winopts = {
+      width = 0.5,
+      -- height is number of items, with a max of 80% screen height
+      height = math.floor(math.min(vim.o.lines * 0.8, #items + 2) + 0.5),
+    },
+  })
 end)
 
 vim.keymap.set("n", "<C-p>", "<cmd>FzfLua files<cr>")
 vim.keymap.set("n", "<C-S-p>", "<cmd>FzfLua git_files<cr>")
 vim.keymap.set(
-	"n",
-	"<C-\\>",
-	"<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>"
+  "n",
+  "<C-\\>",
+  "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>"
 )
-vim.keymap.set("n", "<C-/>", "<cmd>FzfLua live_grep<cr>")
+vim.keymap.set("n", "<C-g>", "<cmd>FzfLua live_grep<cr>")
 vim.keymap.set("n", "<C-f>", "<cmd>FzfLua grep_cword<cr>")
 vim.keymap.set("n", "<C-S-f>", "<cmd>FzfLua grep_cWORD<cr>")
 vim.keymap.set("v", "<C-f>", "<cmd>FzfLua grep_visual<cr>")
