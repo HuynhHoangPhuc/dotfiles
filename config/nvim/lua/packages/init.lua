74e0005f6a7a3a856699c75697cc351f647acac3
Huynh Hoang Phuc <huynhhoangphuc@proton.me>
2026-03-19 04:59:26 +0000

    fix(nvim): read tool list from languageconfigs instead of conform/lint
    conform.list_all_formatters() and lint.linters_by_ft were empty when
    mason refresh callback fired before plugin/ files loaded conform/lint.
    Read directly from languageconfigs which is available immediately.

--- config/nvim/lua/packages/init.lua
+++ config/nvim/lua/packages/init.lua
@@ -5,34 +5,23 @@ local pkgs = {}
 
 M.get_pkgs = function()
 	local tools = {}
+	local langconfigs = require("languageconfigs")
 
-	local native_lsps = vim.tbl_keys(vim.lsp._enabled_configs or {})
-	local lspconfig_lsps = require("lspconfig.util").available_servers()
-	vim.list_extend(tools, lspconfig_lsps)
-	vim.list_extend(tools, native_lsps)
+	-- LSP servers from languageconfigs (source of truth)
+	vim.list_extend(tools, vim.tbl_keys(langconfigs.lsp.servers))
 
-	local conform_exists, conform = pcall(require, "conform")
-
-	if conform_exists then
-		for _, v in ipairs(conform.list_all_formatters()) do
-			local fmts = vim.split(v.name:gsub(",", ""), "%s+")
-			vim.list_extend(tools, fmts)
-		end
+	-- Formatters from languageconfigs
+	for _, fmts in pairs(langconfigs.format.formatters_by_ft) do
+		vim.list_extend(tools, fmts)
 	end
 
-	-- nvim-lint
-	local lint_exists, lint = pcall(require, "lint")
-
-	if lint_exists then
-		local linters = lint.linters_by_ft
-
-		for _, v in pairs(linters) do
-			vim.list_extend(tools, v)
-		end
+	-- Linters from languageconfigs
+	for _, lints in pairs(langconfigs.lint.linters_by_ft) do
+		vim.list_extend(tools, lints)
 	end
 
 	for _, v in pairs(tools) do
-		if not vim.list_contains(pkgs, mason_names[v]) then
+		if mason_names[v] and not vim.list_contains(pkgs, mason_names[v]) then
 			table.insert(pkgs, mason_names[v])
 		end
 	end
