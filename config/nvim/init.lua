acf3c76f5b86c493eae03e2d3cda1531da03a434
Huynh Hoang Phuc <huynhhoangphuc@proton.me>
2026-08-04 11:15:09 +0700

    fix(nvim): treat neovim on Windows use git bash instead of cmd.exe

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,3 +6,11 @@ if not os.getenv("DOTFILES_WITH_FLAKE") then
 	require("plugins")
 	require("packages").install_all()
 end
+
+if vim.fn.has("win32") == 1 then
+	-- Detects if the current shell is bash
+	if string.find(vim.o.shell, "bash") or string.find(vim.o.shell, "sh") then
+		vim.o.shellcmdflag = "-c"
+		vim.o.shellxquote = ""
+	end
+end
